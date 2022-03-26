4f4c83a0b85d449c1dda805f5b80a0f6bc5dfadb
khushboobabil <99880847+khushboobabil@users.noreply.github.com>
2022-03-26 14:29:58 +0530

    Update internController.js

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,6 +1,7 @@
 const collegeModel = require("../models/collegeModel")
 const internModel = require("../models/internModels")
-const ObjectId = require("mongoose").Types.ObjectId;
+
+
 
 // validation//
 
@@ -14,63 +15,59 @@ const isValid = function (value) {
 // create intern//
 
 const createIntern = async function (req, res) {
-  try {
+try
+  {let data = req.body;
 
-    let data = req.body;
-
-    if (Object.keys(data).length > 0) {
-
-
-      if (!isValid(data.email)) { return res.status(400).send({ status: false, msg: "Email is required" }) }
-      if (!isValid(data.name)) { return res.status(400).send({ status: false, msg: "First name is required" }) }
-      if (!isValid(data.collegeId)) { return res.status(400).send({ status: false, msg: "College Id is required" }) }
+  if (Object.keys(data).length > 0) {
 
 
-
-      if (!ObjectId.isValid(data.collegeId)) { return res.status(400).send({ status: false, msg: "Please provide a valid College Id" }) }
-
-
-
-      let collegeCheckId = await collegeModel.findOne({ _id: data.collegeId, isDeleted: false })
-      if (!collegeCheckId) { return res.status(400).send({ msg: "college you are looking for internship, does not exist" }) }
-
-
-      if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(data.email))) {
-        return res.status(400).send({ status: false, msg: "Please provide a valid email" })
-      }
-      if (!(/^[6-9]\d{9}$/.test(data.mobile))) {
-        return res.status(400).send({ status: false, msg: "Please provide a valid Moblie Number" })
-      }
-
-
-
-      let dupli = await internModel.findOne({ email: data.email })
-      if (dupli) { return res.status(400).send({ status: false, msg: "Email already exists" }) }
-
-
-      let dupliMobile = await internModel.findOne({ mobile: data.mobile })
-      if (dupliMobile) { return res.status(400).send({ status: false, msg: "Mobile Number already exists" }) }
-
-      let savedData = await internModel.create(data);
-      let expectedResult = {
-        isDeleted: savedData.isDeleted,
-        name: savedData.name,
-        email: savedData.email,
-        mobile: savedData.mobile,
-        collegeId: savedData.collegeId
-      }
-      return res.status(201).send({ status: true, internDetails: expectedResult });
-
-    } else {
-      return res.status(400).send({ ERROR: "BAD REQUEST" })
+    if (!isValid(data.email)) { return res.status(400).send({ status: false, msg: "Email is required" }) }
+    if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(data.email))) {
+      return res.status(400).send({ status: false, msg: "Please provide a valid email" })
     }
 
-  } catch (err) {
+    if (!isValid(data.name)) { return res.status(400).send({ status: false, msg: "First name is required" }) }
 
-    return res.status(500).send({ ERROR: err.message })
+    if (!isValid(data.mobile)) { return res.status(400).send({ status: false, msg: "Mobile number is required" }) }
 
-  }
+    if (!(/^[6-9]\d{9}$/.test(data.mobile))) {
+      return res.status(400).send({ status: false, msg: "please provide a valid moblie Number" })
+    }
+
+    if (!isValid(data.collegeName)) { return res.status(400).send({ status: false, msg: "College name is required" }) }
+
+    let dupli = await internModel.findOne({ email: data.email })
+
+    if (dupli) { return res.status(400).send({ status: false, msg: "Email already exists" }) }
+
+
+    let dupliMobile = await internModel.findOne({ mobile: data.mobile })
+
+    if (dupliMobile) { return res.status(400).send({ status: false, msg: "Mobile Number already exists" }) }
+
+    const collegeName = data.collegeName
+    
+    const collegeId = await collegeModel.findOne({ name: collegeName })
+    
+    const newDataCollege = collegeId._id
+    
+
+    const savedData = { name: data.name, email: data.email, mobile: data.mobile, collegeId: newDataCollege }
+    const resultIntern = await internModel.create(savedData)
+    res.status(201).send(resultIntern)
+
+
+  } else {
+    return res.status(400).send({ ERROR: "BAD REQUEST" })
+  }}
+
+ catch (err) {
+
+  return res.status(500).send({ ERROR: err.message })
+
+}
 }
 
 
-module.exports.createIntern = createIntern;
+
+module.exports.createIntern = createIntern
