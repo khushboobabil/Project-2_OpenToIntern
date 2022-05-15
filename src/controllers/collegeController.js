0ead49e7b328e9cabbe207c1428ffd29414b714f
khushboobabil <99880847+khushboobabil@users.noreply.github.com>
2022-05-15 10:27:04 +0530

    Update collegeController.js

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -6,6 +6,7 @@ const internModels = require("../models/internModels")
 const isValid = function (value) {
   if (typeof value == undefined || value == null) return false
   if (typeof value === 'string' && value.trim().length === 0) return false
+   if (typeof value === Number && value.trim().length === 0) return false
   return true
 }
 
