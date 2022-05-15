4b9d5cbeb5154339a738f6fd7f8e3af92b26131f
khushboobabil <99880847+khushboobabil@users.noreply.github.com>
2022-05-15 10:27:33 +0530

    Update internController.js

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -8,6 +8,7 @@ const internModel = require("../models/internModels")
 const isValid = function (value) {
   if (typeof value == undefined || value == null) return false
   if (typeof value === 'string' && value.trim().length === 0) return false
+  if (typeof value === Number && value.trim().length === 0) return false
   return true
 }
 
